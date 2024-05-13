18778a4042ed0a492babbf2ec8dd379013c71160
DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-05-13 20:00:24 +0900

    커밋한 사람: DESKTOP-IFJTLG3\USER 2024.05.07 전체적인 틀 타이머 구현

---
+++ Brace_for_Impact_project_dev/Time.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <Windows.h>
+
+class Time
+{
+public:
+	static void Initailize ( );
+	static void Update ( );
+	static void Render ( HDC hdc );
+
+	static float DeltaTime ( ) { return DeltaTimeValue; }
+
+private:
+	static LARGE_INTEGER CpuFrequency;
+	static LARGE_INTEGER PrevFrequency;
+	static LARGE_INTEGER CurrentFrequency;
+	static float DeltaTimeValue;
+};
