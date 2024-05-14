95caed6e7f3165cc7e404f606bceb01e4e63e2d4
DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-05-14 20:57:51 +0900

    전체적인 틀 사용자 지정 타이머 구현

---
+++ Brace_for_Impact_project_dev/Timer.cpp
@@ -0,0 +1,83 @@
+﻿#include "Timer.h"
+
+std::vector<Timer::timer_ID>Timer::timerID = {};
+void Timer::Settimer (  UINT create_id , float settigtimer) {
+	timer_ID temp;
+	temp.ID = create_id;
+	temp.time = 0;
+	temp.want_time = settigtimer;
+	timerID.push_back (temp);
+}
+
+float Timer::Peektimer ( UINT id ) {
+
+	for ( int i = 0; i < timerID.size ( ); i++ ) {
+		if ( timerID[ i ].ID == id ) {
+			
+				return timerID[i].time;
+			
+			
+		}
+
+	}
+}
+
+int Timer::Size ( ) {
+	return timerID.size ( );
+}
+
+bool Timer::Checktimer ( UINT check_id ) {
+
+	for ( int i = 0; i < timerID.size ( ); i++ ) {
+		if ( timerID[ i ].ID == check_id ) {
+			if ( timerID[ i ].want_time <= timerID[ i ].time ) {
+				timerID[i ].time = 0;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}	
+		
+	}
+}
+
+void Timer::Edittimer ( UINT edit_id,float want_time ) {
+
+	for ( int i = 0; i < timerID.size ( ); i++ ) {
+		if ( timerID[ i ].ID == edit_id ) {
+
+			timerID[ i ].want_time=want_time;
+
+			break;
+		}
+
+	}
+}
+
+void Timer::InitTimer ( UINT edit_id  ) {
+	for ( int i = 0; i < timerID.size ( ); i++ ) {
+		if ( timerID[ i ].ID == edit_id ) {
+
+			timerID[ i ].time = 0;
+
+			return;
+		}
+
+	}
+}
+
+void Timer::Killtimer ( UINT delete_id ) {
+	for ( int i = 0; i < timerID.size ( ); i++ ) {
+		if ( timerID[ i ].ID == delete_id ) {
+		timerID.erase ( timerID.begin ( ) + i );
+		}
+
+	}
+}
+
+void Timer::Updatetimer (float deltaTime) {
+	for ( int i = 0; i < timerID.size ( ); i++ ) {
+		timerID[ i ].time += deltaTime;
+	}
+}
---
+++ Brace_for_Impact_project_dev/Timer.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <Windows.h>
+#include <vector>
+
+
+class Timer
+{
+	typedef struct timer_ID {
+		UINT ID;
+		float time;
+		float want_time;
+
+	}timer_ID;
+private:
+	static std::vector<timer_ID> timerID;
+public:
+	static int Size ( );
+	static float Peektimer ( UINT );
+	static bool Checktimer ( UINT );
+	static void Edittimer ( UINT ,float);
+	static void Settimer (UINT,float );
+	static void Killtimer ( UINT);
+	static void Updatetimer (float );
+	static void InitTimer ( UINT edit_id );
+};
+
