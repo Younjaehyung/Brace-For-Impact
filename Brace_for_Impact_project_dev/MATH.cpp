bc37592400526529fc9798c3fd21b0b14d98d842
이동주 <domgdong98@gmail.com>
2024-05-15 21:38:14 +0900

    bullet(추가중)

--- Brace_for_Impact_project_dev/MATH.cpp
+++ Brace_for_Impact_project_dev/MATH.cpp
@@ -3,3 +3,7 @@
 float length(int x1, int y1, int x2, int y2) {
 	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
 }
+
+double angle(int x1, int y1, int x2, int y2) {
+	return atan2(y1-y2,x1-x2 );
+}
--- Brace_for_Impact_project_dev/MATH.h
+++ Brace_for_Impact_project_dev/MATH.h
@@ -4,3 +4,5 @@
 //double PI = 3.141592;
 
 float length(int x1, int y1, int x2, int y2);
+
+double angle(int x1, int y1, int x2, int y2);
