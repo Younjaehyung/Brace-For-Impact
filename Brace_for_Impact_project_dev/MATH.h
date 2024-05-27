db017ab60b5c0fca62dfe109112d0dada6550637
이동주 <domgdong98@gmail.com>
2024-05-27 18:54:58 +0900

    복구

--- Brace_for_Impact_project_dev/MATH.h
+++ Brace_for_Impact_project_dev/MATH.h
@@ -16,13 +16,7 @@ typedef struct RECTS {
 	float bottom;
 }RECTS;
 
-//원과 사각형 충돌
-BOOL rect2Cir(RECTS rect, double x, double y, int sz);
-//면과 선 충돌
-BOOL rect2Line ( RECTS rect , float p1x , float p1y , float mopx , float mopy );
-
 bool IntersectRect_float(RECTS& ,RECTS&);
-
 
 
 bool PtinRect_float(const RECTS& rect, const POINT& point);
