a3efedc4ab5e607aa7eda00fd02b53a4feac7950
이동주 <domgdong98@gmail.com>
2024-05-30 22:35:44 +0900

    Reapply "Merge branch 'main' into texture_dev"
    This reverts commit 5343a3aea56b9f91f94c2697fe950410fdd30b5f.

---
+++ Brace_for_Impact_project_dev/BLOCK.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <Windows.h>
+#include "input.h"
+#include "MATH.h"
+
+
+class Block
+{
+private:
+	RECTS rect;
+	int type;
+public:
+	Block ( );
+	void Render ( HDC mDC );
+	RECTS ReturnRect ( );
+};
