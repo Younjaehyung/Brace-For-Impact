a1013b749ce24a1fa67e3671b672ce8a328d0060
이동주 <domgdong98@gmail.com>
2024-06-03 16:58:42 +0900

    2024-06-03 // 4:57 몬스터 추가(탄속*50,근접)  탄 맞을시 몬스터 사망

--- Brace_for_Impact_project_dev/BULLET.cpp
+++ Brace_for_Impact_project_dev/BULLET.cpp
@@ -24,27 +24,30 @@ void bullet::move ( ) {
 		if ( x < 0 || y < 0 ) {
 			type = 0;
 		}
-		//벽에 탄 맞음
-		for ( auto& ScanBlock : BlockManager::getInstance ( ).BlockReturn ( ) ) {
-			if ( rect2Cir ( ScanBlock.ReturnRect ( ) , x , y , SIZE ) ) {
-				type = 0;
-			}
-		}
-		//몬스터가 탄 맞음
-		if ( type <= 10 ) {
-			for ( auto& ScanMop : MonsterManager::getInstance ( ).MopReturn ( ) ) {
-				if ( rect2Cir ( ScanMop->ReturnRect ( ) , x , y , SIZE ) ) {
+		if ( type > 0 ) {
+			//벽에 탄 맞음
+			for ( auto& ScanBlock : BlockManager::getInstance ( ).BlockReturn ( ) ) {
+				if ( rect2Cir ( ScanBlock.ReturnRect ( ) , x , y , SIZE ) ) {
 					type = 0;
 				}
 			}
-		}
-		if ( type > 10 ) {
-			//플레이어(탱크)가 탄 맞음
-			RECTS tankrect = PlayerManager::getInstance ( ).Tank_return ( ).ReturnRect ( );
-			tankrect.bottom = tankrect.top + 192;
-			tankrect.right = tankrect.left + 192;
-			if ( rect2Cir ( tankrect , x , y , SIZE ) ) {
-				type = 0;
+			//몬스터가 탄 맞음
+			if ( type>0 &&type < 10 ) {
+				for ( auto& ScanMop : MonsterManager::getInstance ( ).MopReturn ( ) ) {
+					if ( rect2Cir ( ScanMop->ReturnRect ( ) , x , y , SIZE ) && ScanMop->ReturnHP ( ) > 0 ) {
+						type = 0;
+						ScanMop->Damage ( 1 );
+					}
+				}
+			}
+			if ( type >= 10 ) {
+				//플레이어(탱크)가 탄 맞음
+				RECTS tankrect = PlayerManager::getInstance ( ).Tank_return ( ).ReturnRect ( );
+				tankrect.bottom = tankrect.top + 192;
+				tankrect.right = tankrect.left + 192;
+				if ( rect2Cir ( tankrect , x , y , SIZE ) ) {
+					type = 0;
+				}
 			}
 		}
 	
--- Brace_for_Impact_project_dev/MONSTER.cpp
+++ Brace_for_Impact_project_dev/MONSTER.cpp
@@ -1,7 +1,7 @@
 ﻿#include "MONSTER.h"
 
 //std::list<mop*> MonsterManager::mops;
-int MONSTERLEN = 200;
+int MONSTERLEN = 250;
 int MOPSIZE = 20;
 int BLOCKCOUNT = 20;
 
@@ -12,7 +12,15 @@ std::uniform_int_distribution<int> sponsemop(0, 800);
 mop::mop(int type) {
 	mop_inform.x = sponsemop ( genmop );
 	mop_inform.y = sponsemop ( genmop );
-	mop_inform.hp = 100;
+	if ( type == 1 ) {
+		mop_inform.hp = 2;
+	}
+	else if ( type == 2 ) {
+		mop_inform.hp = 1;
+	}
+	else if ( type == 3 ) {
+		mop_inform.hp = 1;
+	}
 	mop_inform.type = type;
 	mop_inform.cnt = 0;
 	attack_count = 0;
@@ -28,11 +36,23 @@ void mop::attack( Tank& p1) {
 		}
 		else if ( mop_inform.type == 2 ) {
 
-			double targetx = ( double ) ( p1.ReturnRect ( ).left + 20 );
-			double targety = ( double ) ( p1.ReturnRect ( ).top + 20 );
+			double targetx = ( double ) ( p1.ReturnRect ( ).left + 60 );
+			double targety = ( double ) ( p1.ReturnRect ( ).top + 60 );
 			double ang = angle ( ( double ) ( mop_inform.x ) , ( double ) ( mop_inform.y ) , targetx , targety );
 			bullet* newbullet = new bullet ( mop_inform.x , mop_inform.y , 10 , -cos ( ang ) , -sin ( ang ) );
 			BulletManager::getInstance().CreateBullet ( newbullet );
+
+		}
+		attack_count = 0;
+	}
+	if ( attack_count >= 0.1 ) {
+		if ( mop_inform.type == 3 ) {
+
+			double targetx = ( double ) ( p1.ReturnRect ( ).left + 60 );
+			double targety = ( double ) ( p1.ReturnRect ( ).top + 60 );
+			double ang = angle ( ( double ) ( mop_inform.x ) , ( double ) ( mop_inform.y ) , targetx , targety );
+			bullet* newbullet = new bullet ( mop_inform.x , mop_inform.y , 10 , -cos ( ang ) , -sin ( ang ) );
+			BulletManager::getInstance ( ).CreateBullet ( newbullet );
 
 		}
 		attack_count = 0;
@@ -70,24 +90,35 @@ void mop::move ( Tank& p1  ) {
 	if ( mop_inform.type == 1 ) {
 	
 		if ( frame >= 6 ) frame = 0;
-		if ( p1.ReturnRect ( ).left < mop_inform.x ) {
-			mop_inform.x -= speed;
-			direct = 3;
+		if ( blockmop ) {
+			if ( cpyrect.top > moprect.bottom ) {
+				mop_inform.x += speed;
+			}
+			if ( cpyrect.bottom < moprect.top ) {
+				mop_inform.x -= speed;
+			}
+			if ( cpyrect.right < moprect.left ) {
+				mop_inform.y += speed;
+			}
+			if ( cpyrect.left > moprect.right ) {
+				mop_inform.y -= speed;
+			}
 		}
-
-		if ( p1.ReturnRect ( ).top < mop_inform.y ) {
-			mop_inform.y -= speed;
-			direct = 0;
-		}
-
-		if ( p1.ReturnRect ( ).left > mop_inform.x ) {
-			mop_inform.x += speed;
-
-		}
-
-		if ( p1.ReturnRect ( ).top > mop_inform.y ) {
-			mop_inform.y += speed;
-
+		else {
+			if ( p1.ReturnRect ( ).left + 20 < mop_inform.x ) {
+				mop_inform.x -= speed;
+				direct = 3;
+			}
+			else {
+				mop_inform.x += speed;
+				direct = 0;
+			}
+			if ( p1.ReturnRect ( ).top + 20 < mop_inform.y ) {
+				mop_inform.y -= speed;
+			}
+			else {
+				mop_inform.y += speed;
+			}
 		}
 
 
@@ -110,7 +141,52 @@ void mop::move ( Tank& p1  ) {
 			}
 		}
 		else {
-			if ( length ( p1.ReturnRect ( ).left + 20 , p1.ReturnRect ( ).top + 20 , mop_inform.x , mop_inform.y ) > MONSTERLEN ) {
+			if ( length ( p1.ReturnRect ( ).left + 60 , p1.ReturnRect ( ).top + 60 , mop_inform.x , mop_inform.y ) > MONSTERLEN ) {
+
+				if ( p1.ReturnRect ( ).left + 20 < mop_inform.x ) {
+					mop_inform.x -= speed;
+					direct = 3;
+				}
+				else {
+					mop_inform.x += speed;
+					direct = 0;
+				}
+				if ( p1.ReturnRect ( ).top + 20 < mop_inform.y ) {
+					mop_inform.y -= speed;
+				}
+				else {
+					mop_inform.y += speed;
+				}
+				/*
+				if (p1.f_ReturnRect().left > p->x) {
+					p->x += speed;
+				}
+				if (p1.f_ReturnRect().top > p->y) {
+					p->y += speed;
+				}
+				*/
+			}
+		}
+	}
+	else if ( mop_inform.type == 3 ) {
+
+		if ( frame >= 4 ) frame = 0;
+		if ( blockmop ) {
+			if ( cpyrect.top > moprect.bottom ) {
+				mop_inform.x += speed;
+			}
+			if ( cpyrect.bottom < moprect.top ) {
+				mop_inform.x -= speed;
+			}
+			if ( cpyrect.right < moprect.left ) {
+				mop_inform.y += speed;
+			}
+			if ( cpyrect.left > moprect.right ) {
+				mop_inform.y -= speed;
+			}
+		}
+		else {
+			if ( length ( p1.ReturnRect ( ).left + 60 , p1.ReturnRect ( ).top + 60 , mop_inform.x , mop_inform.y ) > MONSTERLEN ) {
 
 				if ( p1.ReturnRect ( ).left + 20 < mop_inform.x ) {
 					mop_inform.x -= speed;
@@ -149,9 +225,10 @@ void mop::move ( Tank& p1  ) {
 }
 
 void mop::Update( ){
-	move(PlayerManager::getInstance().Tank_return());
-	attack( PlayerManager::getInstance ( ).Tank_return ( ) );
-
+	
+	move ( PlayerManager::getInstance ( ).Tank_return ( ) );
+	attack ( PlayerManager::getInstance ( ).Tank_return ( ) );
+	
 }
 
 void mop::Render( const HDC& dc) {
@@ -162,9 +239,19 @@ void mop::Render( const HDC& dc) {
 		oldBrush = (HBRUSH)SelectObject(dc, hBrush);
 
 		//OSW 적 가죽1
-		Rectangle(dc, mop_inform.x - MOPSIZE, mop_inform.y - MOPSIZE, mop_inform.x + MOPSIZE, mop_inform.y + MOPSIZE);
-		TransparentBlt ( dc , mop_inform.x - MOPSIZE , mop_inform.y - MOPSIZE ,128 , 128 ,
-		Texture::getInstance ( ).Texture_GetDC ( "B_Enemy_2" ) , frame * 64 , direct * 64, 64 , 64 , RGB ( 255 , 255 , 255 ) );
+		if ( mop_inform.type == 1 ) {
+			Rectangle ( dc , mop_inform.x - MOPSIZE , mop_inform.y - MOPSIZE , mop_inform.x + MOPSIZE , mop_inform.y + MOPSIZE );
+			//몬스터 추가해줘 응애
+		}
+		else if ( mop_inform.type == 2 ) {
+			//Rectangle ( dc , mop_inform.x - MOPSIZE , mop_inform.y - MOPSIZE , mop_inform.x + MOPSIZE , mop_inform.y + MOPSIZE );
+			TransparentBlt ( dc , mop_inform.x - MOPSIZE , mop_inform.y - MOPSIZE , 128 , 128 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Enemy_2" ) , frame * 64 , direct * 64 , 64 , 64 , RGB ( 255 , 255 , 255 ) );
+		}
+		else if ( mop_inform.type == 3 ) {
+			Ellipse( dc , mop_inform.x  , mop_inform.y  , mop_inform.x + 60 , mop_inform.y + 60 );
+			//몬스터 추가해줘 응애
+		}
 		//
 
 		
@@ -176,6 +263,14 @@ void mop::Render( const HDC& dc) {
 RECTS mop::ReturnRect ( ) {
 	RECTS r = { mop_inform.x,mop_inform.y, mop_inform.x+100 , mop_inform.y+100 };
 	return r;
+}
+
+int mop::ReturnHP ( ) {
+	return mop_inform.hp;
+}
+
+void mop::Damage ( int damage ) {
+	mop_inform.hp -= damage;
 }
 
 
@@ -191,14 +286,18 @@ void MonsterManager::spawn ( int type ) {
 
 void MonsterManager::Update (  )
 {
-	for ( auto iter : mops ) {
-		iter->Update (  );
 
+	for ( auto iter : mops ) {
+		if ( iter->ReturnHP ( ) > 0 ) {
+			iter->Update ( );
+		}
 	}
 
 	if ( count >= 10 ) {
 		count = 0;
 		spawn ( 2 );
+		spawn ( 1 );
+		spawn ( 3 );
 		
 	}
 	count += Time::DeltaTime ( );
@@ -208,7 +307,9 @@ void MonsterManager::Update (  )
 void MonsterManager::Render ( const HDC& mDC)
 {
 	for ( auto iter : mops ) {
-		iter->Render(mDC);
+		if ( iter->ReturnHP ( ) > 0 ) {
+			iter->Render ( mDC );
+		}
 	}
 }
 
