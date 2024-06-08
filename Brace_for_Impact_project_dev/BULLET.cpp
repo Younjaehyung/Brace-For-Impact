2087b42fd54add9887ecf9a3a93a1853988e2156
이동주 <domgdong98@gmail.com>
2024-06-08 17:45:49 +0900

    2024-06-08 //5:45  총알 가죽 구분

--- Brace_for_Impact_project_dev/BULLET.cpp
+++ Brace_for_Impact_project_dev/BULLET.cpp
@@ -105,9 +105,19 @@ void bullet::move ( ) {
 }
 void bullet::Render ( const HDC& dc ) {
 	if ( type != 0 ) {
-		TransparentBlt ( dc , x - 4 * SIZE , y - 4 * SIZE , SIZE * 8 , SIZE * 8 ,
-			Texture::getInstance ( ).Texture_GetDC ( "B_Bullet_2" ) , frame * 64 , 1 * 64 , 64 , 64 , RGB ( 255 , 255 , 255 ) );
-		Ellipse ( dc , x - SIZE -counter, y - SIZE -counter , x + SIZE +counter , y + SIZE +counter );
+		if ( PlayerBullet ) { //플레이어가 쏜 총알
+			TransparentBlt ( dc , x - 4 * SIZE , y - 4 * SIZE , SIZE * 8 , SIZE * 8 ,
+				Texture::getInstance ( ).Texture_GetDC ( "B_Bullet_2" ) , frame * 64 , 1 * 64 , 64 , 64 , RGB ( 255 , 255 , 255 ) );
+			Ellipse ( dc , x - SIZE - counter , y - SIZE - counter , x + SIZE + counter , y + SIZE + counter );
+		}
+		else { // 몬스터가 쏜 총알
+			if ( type == 14 ) { // 자폭이 폭발		
+				Ellipse ( dc , x - SIZE - counter , y - SIZE - counter , x + SIZE + counter , y + SIZE + counter );
+			}
+			else { //그 외 다른애들 총알
+				Ellipse ( dc , x - SIZE - counter , y - SIZE - counter , x + SIZE + counter , y + SIZE + counter );
+			}
+		}
 	}
 }
 
