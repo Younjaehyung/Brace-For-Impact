b84d6a1266b9add181a2e9ab74eb45e4ca2db61b
OSW <osw00224@gmail.com>
2024-06-15 17:32:33 +0900

    24.06.15 17:32 // 게임 메뉴에서 게임 룰과 게임 승리 이미지 추가. 몬스터 보스 3추가.

--- Brace_for_Impact_project_dev/Application.cpp
+++ Brace_for_Impact_project_dev/Application.cpp
@@ -31,7 +31,7 @@ void Application::f_Initialize ( HWND hWnd , HINSTANCE  hInst_temp ) {
 	HandleResize ( );
 	gameobject.Initailize ( hDC,g_hinst );
 	//임시 음악
-	PlaySound ( MAKEINTRESOURCE ( IDR_WAVE1 ) , g_hinst , SND_RESOURCE | SND_ASYNC | SND_LOOP );
+	//PlaySound ( MAKEINTRESOURCE ( IDR_WAVE1 ) , g_hinst , SND_RESOURCE | SND_ASYNC | SND_LOOP );
 	
 	GameDC = CreateCompatibleDC ( hDC );
 	mBitmap = CreateCompatibleBitmap ( hDC , 1024 * 2 , 1024 * 2 );
@@ -50,7 +50,7 @@ void Application::f_Render() {
 	//Rectangle ( mDC , r_car.left , r_car.top , r_car.right , r_car.bottom );	//포신 조종실 UI
 	//Rectangle ( mDC , r_cannon.left , r_cannon.top , r_cannon.right , r_cannon.bottom );	//차체 조종실 UI
 	//Rectangle ( mDC , r_playground.left , r_playground.top , r_playground.right , r_playground.bottom );	//차체 조종실 UI
-
+	
 	//SceneManager::Render ( mDC );
 
 	switch ( Stage_num ) {
@@ -124,7 +124,7 @@ void Application::f_Render() {
 	
 gameobject.Camera_UI ( mDC );
 
-	if ( gameStart ) {
+	if ( gameStatus == 0) {
 		//TITLE 출력
 		BitBlt ( mDC , 0 , 0 , 1024 , 1024 ,
 			Texture::getInstance ( ).Texture_GetDC ( "B_TITLE" ) , TankController::ScreenTitle_frame ( ).frame * 1024 , 0 , SRCCOPY );
@@ -138,8 +138,6 @@ gameobject.Camera_UI ( mDC );
 			Texture::getInstance ( ).Texture_GetDC ( "B_Selectframe" ) , 384 * 1 , 0 , 384 , 96 , RGB ( 255 , 255 , 255 ) );
 		TransparentBlt ( mDC , 1024 + 8 + 16 * 8 , 32 * 17 , 384 , 96 ,
 			Texture::getInstance ( ).Texture_GetDC ( "B_Selectframe" ) , 384*2 , 0 , 384 , 96 , RGB ( 255 , 255 , 255 ) );
-
-		
 		
 		if ( input::GetKeyDown ( eKeyCode::S ) ) {
 			if(Cursor < 11 )
@@ -152,18 +150,47 @@ gameobject.Camera_UI ( mDC );
 
 		//게임 시작 창일때
 		if ( input::GetKeyDown ( eKeyCode::F ) && Cursor == 0) {
-			gameStart = !gameStart;
+			gameStatus = 10;
 		}else if (input::GetKeyDown ( eKeyCode::F ) && Cursor == 4) {
-
+			gameStatus = 1;
 		} else if (input::GetKeyDown ( eKeyCode::F ) && Cursor == 8) {
-			exit ( 1 );
+			gameStatus = 2;
 		}
+	}
+	else if ( gameStatus == 1 ) {
+		BitBlt ( mDC , 0 , 0 , 1024 , 1024 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_TITLE" ) , TankController::ScreenTitle_frame ( ).frame * 1024 , 0 , SRCCOPY );
+		BitBlt ( mDC , 1024 - 50 , 0 , 1024 + 656 , 1024 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Select" ) , 0 , 0 , SRCCOPY );
+		TransparentBlt ( mDC , 1024 + 8 + 16 * 8 , 32 * ( 9) , 384 , 96 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Selectframe" ) , 0 , 0 , 384 , 96 , RGB ( 255 , 255 , 255 ) );
+		TransparentBlt ( mDC , 1024 + 8 + 16 * 8 , 32 * 13 , 384 , 96 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Selectframe" ) , 384 * 1 , 0 , 384 , 96 , RGB ( 255 , 255 , 255 ) );
+		TransparentBlt ( mDC , 1024 + 8 + 16 * 8 , 32 * 17 , 384 , 96 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Selectframe" ) , 384*2 , 0 , 384 , 96 , RGB ( 255 , 255 , 255 ) );
+		
+		TransparentBlt ( mDC , 0, 0 , 1024 , 768 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Rule" ), 0,0,1024 , 768 ,RGB(255,255,255) );
+		if ( input::GetKeyDown ( eKeyCode::F )) {
+			gameStatus = 0;
+		}
+	}
 
+	else if ( gameStatus == 2 ) {
+
+		Rectangle ( mDC , 0 , 0 , rt.right , rt.bottom );
+		TransparentBlt ( mDC , 128*5 , 256 , 320 , 320 ,
+			Texture::getInstance ( ).Texture_GetDC ( "B_Clear" ) , 0 , 0 , 320 , 320 , RGB ( 255 , 255 , 255 ) );
+		TransparentBlt ( mDC , 128 * 8 , 128*5 , PLAYERSIZE , PLAYERSIZE , Texture::getInstance ( ).Texture_GetDC ( "B_Player_2" ) , 0 ,64 * 2 , 64 , 64 , RGB ( 255 , 255 , 255 ) );
+		TransparentBlt ( mDC , 128 * 4 , 128*5 , PLAYERSIZE , PLAYERSIZE , Texture::getInstance ( ).Texture_GetDC ( "B_Player_1" ) , 0 , 64 * 7 , 64 , 64 , RGB ( 255 , 255 , 255 ) );
+		if ( input::GetKeyDown ( eKeyCode::F ) ) {
+			gameStatus = 0;
+		}
 	}
 
 	
 	//플레이어 활동 가능 구역
-	//Rectangle ( mDC , 1024 + 100 , 0 + 150 , 1024 + 550 , 0 + 100 + 800 );
+	//Rectangle ( mDC , 1024 + 16*6 , 0 + 16*9 , 1024 + 16*35 , 0 + 100 + 16*50 );
 
 	// 
 	//===OSW===
